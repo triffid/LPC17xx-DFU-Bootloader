6a19136a94fe247e097c655e99155287b13a2634
Michael Moon <triffid.hunter@gmail.com>
2012-11-27 22:30:56 +1100

    use direct register manipulation to configure UART, saves _heaps_ of flash space

--- uart.c
+++ uart.c
@@ -134,6 +134,89 @@ void UART_pin_init(PinName rxpin, PinName txpin)
 //     irqrouter[port] = this;
 }
 
+static uint32_t const uabs(const uint32_t a, const uint32_t b)
+{
+	if (a>=b)
+		return a-b;
+	return b-a;
+}
+
+static uint32_t const calc_baud(uint32_t pclk, uint32_t dl, uint32_t divaddval, uint32_t mulval)
+{
+	// 65535 * 14 * 16 is less than 2**24 so we have a spare 8 bits of precision
+	// we can use them to increase our accuracy quite a bit
+	// pclk is less than 2**27, so we have 5 spare bits for the numerator
+	// this means we can do (numerator * 2**5) / (denominator * 2**8) * 2**3 to get the highest accuracy possible with 32 bit integers
+	// denominator is 16 * (dl * (1 + (divadd / mul)) which can be expanded to
+	// dl*16 + dl*16*divadd/mul which gives far more opportunity for using all our precision
+	uint32_t dx = ((dl * 16 * 32 * 8) + ((dl * 16 * divaddval * 32 * 8) / mulval));
+	return ((pclk * 32) / dx) * 8;
+}
+
+typedef struct {
+	uint32_t	baud;
+	uint8_t	pd;
+	uint16_t	dl;
+	uint32_t	dx;
+	uint8_t	mulval;
+	uint8_t	divaddval;
+} uart_regs;
+
+static int baud_space_search(uint32_t target_baud, uart_regs *r)
+{
+	uint32_t pd, dl, mulval, divaddval;
+	int i = 0;
+	r->baud = 0;
+	for (pd = ((target_baud < 1000000)?3:1); pd < 4; pd--)
+	{
+		uint32_t pclk = SystemCoreClock / (1<<pd);
+		for (mulval = 1; mulval < 16; mulval++)
+		{
+			for (divaddval = 0; divaddval < mulval; divaddval++)
+			{
+				i++;
+				// baud = pclk / (16 * dl * (1 + (DivAdd / Mul))
+				// solving for dl, we get dl = mul * pclk / (16 * baud * (divadd + mul))
+				// we double the numerator, add 1 to the result then halve to effectivel round up when dl % 1 > 0.5
+				dl = (((2 * mulval * pclk) / (16 * target_baud * (divaddval + mulval))) + 1) / 2;
+
+				// dl is a 16 bit field, if result needs more then we search again
+				if (dl > 65535)
+					continue;
+
+				// datasheet says if DLL==DLM==0, then 1 is used instead since divide by zero is ungood
+					if (dl == 0)
+						dl = 1;
+
+					// datasheet says if DIVADDVAL > 0 then DL must be >= 2
+						if ((divaddval > 0) && (dl < 2))
+							dl = 2;
+
+						uint32_t b = calc_baud(pclk, dl, divaddval, mulval);
+						if (uabs(b, target_baud) < uabs(r->baud, target_baud))
+						{
+							r->baud      = b;
+							r->pd        = pd;
+							r->dl        = dl;
+							r->dx       = ((dl * 16 * 32 * 8) + ((dl * 16 * divaddval * 32 * 8) / mulval));
+							r->mulval    = mulval;
+							r->divaddval = divaddval;
+							// 					printf("\t\t{%7d,%4d,%6d,%3d,%3d},\t// Actual baud: %7d, error %c%4.2f%%, %d iterations\n", target_baud, 1<<best.pd, best.dl, best.mulval, best.divaddval, b, ((b > target_baud)?'+':((b < target_baud)?'-':' ')), (uabs(target_baud, b) * 100.0) / target_baud, i);
+							if (b == target_baud)
+								return i;
+							// within 0.08%
+								if ((uabs(r->baud, target_baud) * 1536 / target_baud) < 1)
+									return i;
+						}
+			}
+		}
+		// don't check higher pclk if we're within 0.5%
+		if ((uabs(r->baud, target_baud) * 200 / target_baud) < 1)
+			return i;
+	}
+	return i;
+}
+
 int UART_baud(int baud)
 {
     TxIntStat = RESET;
@@ -141,43 +224,111 @@ int UART_baud(int baud)
     RB_ZERO(txbuf);
     RB_ZERO(rxbuf);
 
-    UART_CFG_Type UARTConfigStruct;
-    UART_FIFO_CFG_Type UARTFIFOConfigStruct;
+	uart_regs r = { 0, 0, 0, 0, 0, 0 };
 
-    UART_ConfigStructInit(&UARTConfigStruct);
+	baud_space_search(baud, &r);
 
-    UARTConfigStruct.Baud_rate = baud;
+	uint8_t pclkdiv;
+	IRQn_Type c = 255;
 
-    UART_Init(u, &UARTConfigStruct);
+	switch (r.pd)
+	{
+		case 0:
+			pclkdiv = CLKPWR_PCLKSEL_CCLK_DIV_1;
+			break;
+		case 1:
+			pclkdiv = CLKPWR_PCLKSEL_CCLK_DIV_2;
+			break;
+		case 2:
+			pclkdiv = CLKPWR_PCLKSEL_CCLK_DIV_4;
+			break;
+		default:
+		case 3:
+			pclkdiv = CLKPWR_PCLKSEL_CCLK_DIV_8;
+			break;
+	}
 
-    UART_FIFOConfigStructInit(&UARTFIFOConfigStruct);
+// PCLKSEL0
+#define PCLK_UART0 6
+#define PCLK_UART1 8
+// PCLKSEL1
+#define PCLK_UART2 16
+#define PCLK_UART3 18
 
-    UART_FIFOConfig(u, &UARTFIFOConfigStruct);
+	switch(port)
+	{
+		case 0:
+			LPC_SC->PCONP |= CLKPWR_PCONP_PCUART0;
+			LPC_SC->PCLKSEL0 = (LPC_SC->PCLKSEL0 & ~(3 << PCLK_UART0)) | pclkdiv << PCLK_UART0;
+			c = UART0_IRQn;
+			break;
+		case 1:
+			LPC_SC->PCONP |= CLKPWR_PCONP_PCUART1;
+			LPC_SC->PCLKSEL0 = (LPC_SC->PCLKSEL0 & ~(3 << PCLK_UART1)) | pclkdiv << PCLK_UART1;
+			c = UART1_IRQn;
+			break;
+		case 2:
+			LPC_SC->PCONP |= CLKPWR_PCONP_PCUART2;
+			LPC_SC->PCLKSEL1 = (LPC_SC->PCLKSEL1 & ~(3 << PCLK_UART2)) | pclkdiv << PCLK_UART2;
+			c = UART2_IRQn;
+			break;
+		case 3:
+			LPC_SC->PCONP |= CLKPWR_PCONP_PCUART3;
+			LPC_SC->PCLKSEL1 = (LPC_SC->PCLKSEL1 & ~(3 << PCLK_UART3)) | pclkdiv << PCLK_UART3;
+			c = UART3_IRQn;
+			break;
+	}
 
-    UART_TxCmd(u, ENABLE);
+	u->LCR |= UART_LCR_DLAB_EN;
+	u->DLM = (r.dl >> 8) & 0xFF;
+	u->DLL = (r.dl & 0xFF);
+	u->LCR &= ~(UART_LCR_DLAB_EN) & UART_LCR_BITMASK;
+	u->FDR = (r.divaddval & 0xF) | ((r.mulval & 0xF) << 4);
 
-    UART_IntConfig(u, UART_INTCFG_RBR, ENABLE);
-    UART_IntConfig(u, UART_INTCFG_RLS, ENABLE);
+	u->FCR = (UART_FCR_FIFO_EN | UART_FCR_RX_RS | UART_FCR_TX_RS);
+	u->LCR = UART_LCR_WLEN8;
+	u->ICR = 0;
+	u->TER |= UART_TER_TXEN;
 
-    IRQn_Type c;
-    switch (port) {
-        case 0:
-            c = UART0_IRQn;
-            break;
-        case 1:
-            c = UART1_IRQn;
-            break;
-        case 2:
-            c = UART2_IRQn;
-            break;
-        case 3:
-            c = UART3_IRQn;
-            break;
-        default:
-            return baud;
-    }
+//     UART_CFG_Type UARTConfigStruct;
+//     UART_FIFO_CFG_Type UARTFIFOConfigStruct;
+//
+//     UART_ConfigStructInit(&UARTConfigStruct);
+//
+//     UARTConfigStruct.Baud_rate = baud;
+//
+//     UART_Init(u, &UARTConfigStruct);
+//
+//     UART_FIFOConfigStructInit(&UARTFIFOConfigStruct);
+//
+//     UART_FIFOConfig(u, &UARTFIFOConfigStruct);
+//
+//     UART_TxCmd(u, ENABLE);
+//
+//     UART_IntConfig(u, UART_INTCFG_RBR, ENABLE);
+//     UART_IntConfig(u, UART_INTCFG_RLS, ENABLE);
 
-    NVIC_EnableIRQ(c);
+
+
+//     switch (port) {
+//         case 0:
+//             c = UART0_IRQn;
+//             break;
+//         case 1:
+//             c = UART1_IRQn;
+//             break;
+//         case 2:
+//             c = UART2_IRQn;
+//             break;
+//         case 3:
+//             c = UART3_IRQn;
+//             break;
+//         default:
+//             return baud;
+//     }
+
+	if (c < 128)
+		NVIC_EnableIRQ(c);
 
     return baud;
 }
